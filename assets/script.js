f42523e4a94ca3fdead3c3cb4a62ee880adaea70
Whale Works <oliverfpower@gmail.com>
2023-04-15 16:15:07 -0700

    added a for loop to the javascript file

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,19 @@
-//shes finally here!!!
-var topics = "HTML, CSS, Git, JavaScript";
-console.log(topics)
+// adding if else if statements in order to decide what to study
+var topic = ["HTML", "CSS", "Git", "JavaScript"] 
+
+for (var y = 0; y < topic.length; y++) {
+    console.log(topic[y])
+}
+/*
+if (topic === "HTML") {
+    console.log("Let's study HTML!")
+} else if (topic === "CSS") {
+    console.log("Let's study CSS!")
+} else if (topic === "Git") {
+    console.log("Let's study Git!")
+} else if (topic === "JavaScript") {
+    console.log("Let's study JavaScript!")
+} else {
+    console.log("Please try again!")
+}
+*/
