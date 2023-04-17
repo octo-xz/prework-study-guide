fda839d6bd565f9a08b25d9e5e0e1a91d48de8ab
Whale Works <oliverfpower@gmail.com>
2023-04-17 12:03:30 -0700

    Add functions to js and notes to html

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,30 @@
-// adding if else if statements in order to decide what to study
-var topic = ["HTML", "CSS", "Git", "JavaScript"] 
+var topics = ["HTML", "CSS", "Git", "JavaScript"] 
 
-for (var y = 0; y < topic.length; y++) {
-    console.log(topic[y])
+var randomTopic = topics[Math.floor(Math.random() * topics.length)]
+console.log("These are the topics we covered briefly during the prework")
+// for loop to log the different topics to study
+function listTopics() {
+    for (var y = 0; y < topics.length; y++) {
+        console.log(topics[y])
+    }
 }
-/*
-if (topic === "HTML") {
-    console.log("Let's study HTML!")
-} else if (topic === "CSS") {
-    console.log("Let's study CSS!")
-} else if (topic === "Git") {
-    console.log("Let's study Git!")
-} else if (topic === "JavaScript") {
-    console.log("Let's study JavaScript!")
-} else {
-    console.log("Please try again!")
+
+
+
+function selectTopic() {
+    if (randomTopic === "HTML") {
+        console.log("Let's study HTML!")
+    } else if (randomTopic === "CSS") {
+        console.log("Let's study CSS!")
+    } else if (randomTopic === "Git") {
+        console.log("Let's study Git!")
+    } else if (randomTopic === "JavaScript") {
+        console.log("Let's study JavaScript!")
+    } else {
+        console.log("Please try again!")
+    }
 }
-*/
+
+listTopics();
+console.log("What topic should I study first?")
+selectTopic();
